092668e5aa758dd6b637171eff6a3dfceec3c663
dazep01 <cs.dazep@gmail.com>
2026-06-08 18:34:19 +0700

    Update icons.js

--- apps/ascode/assets/fonts/icons.js
+++ apps/ascode/assets/fonts/icons.js
@@ -3,26 +3,33 @@
     style.innerHTML = `
         @font-face {
             font-family: 'RaaIcons';
-            /* Cukup arahkan ke file .woff saja */
-            src: url('./icomoon.woff') format('woff');
+            src: url('./icomoon.eot?q3se2a');
+            src: url('./icomoon.eot?q3se2a#iefix') format('embedded-opentype'),
+                 url('./icomoon.ttf?q3se2a') format('truetype'),
+                 url('./icomoon.woff?q3se2a') format('woff'),
+                 url('./icomoon.svg?q3se2a#icomoon') format('svg');
             font-weight: normal;
             font-style: normal;
+            font-display: block;
         }
 
-        .raa {
+        /* Menggunakan selector [class^="raa-"] agar otomatis untuk semua ikon raa */
+        [class^="raa-"], [class*=" raa-"] {
             font-family: 'RaaIcons' !important;
-            speak: none;
+            speak: never;
             font-style: normal;
             font-weight: normal;
             font-variant: normal;
             text-transform: none;
             line-height: 1;
             -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
             display: inline-block;
         }
 
-        .raa-copilot::before {
-            content: "\\e900"; 
+        /* Class untuk ikon Anda */
+        .raa-copilot:before {
+            content: "\e900";
         }
     `;
     document.head.appendChild(style);
